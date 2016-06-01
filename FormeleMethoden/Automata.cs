2664a4c75ee3643f2d6e77f7d1081a21fb0fdab3
Jeroen van den Bergh <jeroenvandenbergh1996@hotmail.com>
2016-06-01 10:26:24 +0200

    Basic setup

---
+++ FormeleMethoden/Automata.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormeleMethoden
+{
+    class Automata
+    {
+        private List<string> symbols = new List<string>();
+        private List<string> startStates = new List<string>();
+        private List<string> finalStates = new List<string>();
+        private List<Transition> transitions = new List<Transition>();
+
+        public Automata()
+        {
+            Console.WriteLine("Initialize your Automata, if you need help type help");
+
+            Main();
+        }
+
+        public Automata(List<string> alphabet)
+        {
+            this.symbols = alphabet;
+        }
+
+        public void Main()
+        {
+            switch (Console.ReadLine())
+            {
+                case "print":
+                    PrintCollection();
+                    break;
+                case "help":
+                    Help();
+                    break;
+                case "init":
+                    Init();
+                    break;
+
+                default:
+                    Console.WriteLine("Command not recognized");
+                    break;
+            }
+            Console.ReadLine();
+            Main();
+        }
+
+        public void Init()
+        {
+            Console.WriteLine("Initializing collection");
+            Console.WriteLine("Fill in the alpabetic letters one by one followed by an enter, if you're done type 'end'");
+            SetAlphabet();
+            Console.WriteLine("Alphabet is now set");
+            Main();
+
+
+        }
+
+        public void SetAlphabet()
+        {            
+            //Check if string is null or empty
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {                   
+                    break;
+                }
+
+                symbols.Add(input);
+            }
+        }
+
+        public void AddTransition(Transition transition)
+        {
+            transitions.Add(transition);
+        }
+
+        public void PrintCollection()
+        {
+            foreach (string x in symbols)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        public void Help()
+        {
+
+        }
+
+        public void DefineAsStartState(string t)
+        {
+            startStates.Add(t);
+        }
+
+        public void DefineAsFinalState(string t)
+        {
+            finalStates.Add(t);
+        }
+    }
+}
--- FormeleMethoden/Program.cs
+++ FormeleMethoden/Program.cs
@@ -10,6 +10,50 @@ namespace FormeleMethoden
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("type 'example' to run the test program or type 'manual' to start the manual configuration");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "example":
+                    Example();
+                    break;
+
+                case "manual":
+                    new Automata();
+                    break;
+            }        
         }
+
+        public static void Example()
+        {
+            List<string> alphabet = new List<string> { "a", "b" };
+            Automata m = new Automata(alphabet);
+
+            m.AddTransition(new Transition("q0", 'a', "q1"));
+            m.AddTransition(new Transition("q0", 'b', "q4"));
+
+            m.AddTransition(new Transition("q1", 'a', "q4"));
+            m.AddTransition(new Transition("q1", 'b', "q2"));
+
+            m.AddTransition(new Transition("q2", 'a', "q3"));
+            m.AddTransition(new Transition("q2", 'b', "q4"));
+
+            m.AddTransition(new Transition("q3", 'a', "q1"));
+            m.AddTransition(new Transition("q3", 'b', "q2"));
+
+            // the error state, loops for a and b:
+            //m.addTransition(new Transition<String>("q4", 'a'));
+            //m.addTransition(new Transition<String>("q4", 'b'));
+
+            // only on start state in a dfa:
+            m.DefineAsStartState("q0");
+
+            // two final states:
+            m.DefineAsFinalState("q2");
+            m.DefineAsFinalState("q3");
+        }
+            
     }
 }
